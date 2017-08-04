4170f14f8d5cbd35f24371cc6de987c30490fe8d
Thien <tutybukuny@gmail.com>
2017-08-04 17:53:37 +0700

    not finish show menu animation

--- Service/Content/index.js
+++ Service/Content/index.js
@@ -8,8 +8,10 @@
     $('.menu-control').click(function () {
         if (showMenu) {
             $('.menu').css("display", "none");
+            //            $('.menu').css({"visibility": "hidden"}, {"opacity": 0});
         } else {
             $('.menu').css("display", "block");
+            //            $('.menu').css({ "visibility": "visible" }, { "opacity": 1 });
         }
 
         showMenu = !showMenu;
