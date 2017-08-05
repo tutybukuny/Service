72360d920942aa83276a7b63d3da70c42452278d
Ngọc Thiện <tranhangocthien@gmail.com>
2017-08-05 09:12:57 +0700

    update style

--- Service/Content/index.js
+++ Service/Content/index.js
@@ -1,17 +1,17 @@
 ﻿$(document).ready(function () {
     var showMenu = false;
 
-    $('.dropdown-menu li a').click(function () {
+    $(".dropdown-menu li a").click(function () {
         $("#footer-dropdown").html($(this).text() + ' <span class="glyphicon glyphicon-chevron-down">');
     });
 
-    $('.menu-control').click(function () {
+    $(".menu-control").click(function () {
         if (showMenu) {
-            $('.menu').css("display", "none");
-            //            $('.menu').css({"visibility": "hidden"}, {"opacity": 0});
+            //            $('.menu').css("display", "none");
+            $(".menu").css({ "visibility": "hidden", "opacity": "0" });
         } else {
-            $('.menu').css("display", "block");
-            //            $('.menu').css({ "visibility": "visible" }, { "opacity": 1 });
+            //            $('.menu').css("display", "block");
+            $(".menu").css({ "visibility": "visible", "opacity": "1" });
         }
 
         showMenu = !showMenu;
